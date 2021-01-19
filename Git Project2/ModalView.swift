62c4237e073dc2485b3b3790a4ea11c27c29b762
jeon-ji-yoon <appleru1515@gmail.com>
2021-01-19 20:51:37 +0900

    create project plz

---
+++ Git Project2/ModalView.swift
@@ -0,0 +1,20 @@
+//
+//  ModalView.swift
+//  Git Project2
+//
+//  Created by hjins :) on 2021/01/19.
+//
+
+import SwiftUI
+
+struct ModalView: View {
+    var body: some View {
+        Text("This is the modal view")
+    }
+}
+
+struct ModalView_Previews: PreviewProvider {
+    static var previews: some View {
+        ModalView()
+    }
+}
