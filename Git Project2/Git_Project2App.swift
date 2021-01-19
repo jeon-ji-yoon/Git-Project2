6b9ffdfd9239bf17c541cb6c326fe5607ff8f382
jeon-ji-yoon <appleru1515@gmail.com>
2021-01-19 20:34:06 +0900

    Initial Commit

---
+++ Git Project2/Git_Project2App.swift
@@ -0,0 +1,17 @@
+//
+//  Git_Project2App.swift
+//  Git Project2
+//
+//  Created by hjins :) on 2021/01/19.
+//
+
+import SwiftUI
+
+@main
+struct Git_Project2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
